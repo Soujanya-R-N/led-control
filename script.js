7f38f92012bfb234e9da2d92e001cc94033ed859
Soujanya <rnaiksoujanya@gmail.com>
2026-01-22 16:16:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-const client = mqtt.connect('wss://broker.hivemq.com:8884/mqtt');
+const client = mqtt.connect("wss://broker.hivemq.com:8884/mqtt");
 
-const controlTopic = 'soujanya/led/control';
-const statusTopic  = 'soujanya/led/status';
+// UNIQUE MQTT TOPICS
+const controlTopic = "soujanya/led/control";
+const ledStatusTopic = "soujanya/led/status";
+const deviceStatusTopic = "soujanya/device/status";
 
-const toggle = document.querySelector('input[type="checkbox"]');
-const statusText = document.querySelector('.status');
+const toggle = document.querySelector("input[type='checkbox']");
+const statusText = document.querySelector(".status");
+const deviceStatusText = document.querySelector(".device-status");
 
-client.on('connect', () => {
-  console.log('Connected to MQTT');
-  client.subscribe(statusTopic);
+client.on("connect", () => {
+  console.log("MQTT Connected");
+  client.subscribe(ledStatusTopic);
+  client.subscribe(deviceStatusTopic);
 });
 
-toggle.addEventListener('change', () => {
+// Toggle LED
+toggle.addEventListener("change", () => {
   if (toggle.checked) {
-    client.publish(controlTopic, 'ON');
+    statusText.innerText = "ON";
+    client.publish(controlTopic, "ON");
   } else {
-    client.publish(controlTopic, 'OFF');
+    statusText.innerText = "OFF";
+    client.publish(controlTopic, "OFF");
   }
 });
 
-client.on('message', (topic, message) => {
-  if (topic === statusTopic) {
-    const state = message.toString();
-    statusText.innerText = state;
-    toggle.checked = (state === 'ON');
+// Receive messages
+client.on("message", (topic, message) => {
+  const msg = message.toString();
+
+  if (topic === ledStatusTopic) {
+    statusText.innerText = msg;
+    toggle.checked = (msg === "ON");
+  }
+
+  if (topic === deviceStatusTopic) {
+    deviceStatusText.innerText = "Device: " + msg;
   }
 });
